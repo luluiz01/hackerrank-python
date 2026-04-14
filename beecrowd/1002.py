25dda592abc7f74aa5da2fd10233af4d0245d56a
Luiz Henrique <luiz.pventura@gmail.com>
2026-04-14 18:56:16 -0300

    14/04

---
+++ beecrowd/1002.py
@@ -0,0 +1,8 @@
+n = 3.14159
+
+r = float(input())
+
+a = n * r**2
+
+
+print(f"A={a:.4f}")
---
+++ beecrowd/1005.py
@@ -0,0 +1,6 @@
+a = float(input()) * 3.5
+b = float(input()) * 7.5
+
+
+MEDIA = (a + b) / (3.5 + 7.5)
+print(f"MEDIA = {MEDIA:.5f}")
